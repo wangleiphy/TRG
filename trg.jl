26df1a54a95d4783a027b1d40c3b543b56a3ea3f
Lei Wang <wangleiphy@gmail.com>
2018-10-14 11:05:08 +0800

    should be right now

--- trg.jl
+++ trg.jl
@@ -1,5 +1,5 @@
 using LinearAlgebra:svd, Diagonal
-using TensorOperations
+#using TensorOperations
 
 function TRG(K, Dcut, no_iter)
     D = 2
@@ -14,14 +14,13 @@ function TRG(K, Dcut, no_iter)
     for n in collect(1:no_iter)
         
         #println(n, maximum(T), minimum(T))
-        #maxval = maximum(T)
-        if (n==1)
-            T = T/3
-            lnZ += 2*(no_iter-n+2)*log(3)
-        end
+        maxval = maximum(T)
+        println(n, ", ", maxval)
+        T = T/maxval 
+        lnZ += 2*(no_iter-n+1)*log(maxval)
 
-        D_new = D^2
-        #D_new = min(D^2, Dcut)
+        #D_new = D^2
+        D_new = min(D^2, Dcut)
         inds_new = collect(1:D_new)
 
         Ma = zeros(Float64, D^2, D^2)
@@ -47,13 +46,13 @@ function TRG(K, Dcut, no_iter)
             S4[x, y, m] = sqrt(F.S[m]) * F.Vt[m, x+D*(y-1)]
         end 
 
-        #T_new = zeros(Float64, D_new, D_new, D_new, D_new)
-        #for r in inds_new, u in inds_new, l in inds_new, d in inds_new
-        #    for a in inds, b in inds, g in inds, w in inds
-        #        T_new[r, u, l, d] += S1[w, a, r] * S2[a, b, u] * S3[b, g, l] * S4[g, w, d]
-        #    end
-        #end
-        @tensor T_new[r, u, l, d] := S1[w, a, r] * S2[a, b, u] * S3[b, g, l] * S4[g, w, d]
+        T_new = zeros(Float64, D_new, D_new, D_new, D_new)
+        for r in inds_new, u in inds_new, l in inds_new, d in inds_new
+            for a in inds, b in inds, g in inds, w in inds
+                T_new[r, u, l, d] += S1[w, a, r] * S2[a, b, u] * S3[b, g, l] * S4[g, w, d]
+            end
+        end
+        #@tensor T_new[r, u, l, d] := S1[w, a, r] * S2[a, b, u] * S3[b, g, l] * S4[g, w, d]
 
         D = D_new
         inds = inds_new 
@@ -62,5 +61,5 @@ function TRG(K, Dcut, no_iter)
     lnZ += log(sum(T))
 end
 
-lnZ = TRG(0.44, 6, 2)
+lnZ = TRG(0.44, 16, 3)
 println(exp(lnZ))
